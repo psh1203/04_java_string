1a1b94a3828f525e667fb98c6dc10393f6f1cdd8
toujo <toujo@DESKTOP-1OABBM3>
2021-10-05 23:13:21 +0900

    2021-10-05

--- 04_java_string/src/step4_01/string/StringEx11_정답.java
+++ 04_java_string/src/step4_01/string/StringEx11_정답.java
@@ -1,9 +1,8 @@
 package step4_01.string;
 
-import java.util.Arrays;
 //2021-10-05 4:12 ~ 4:
 import java.util.Scanner;
-
+//2021-10-05 05:40 ~ 11:11
 /*
  * # 쇼핑몰 [관리자]
  * 
@@ -35,22 +34,19 @@ public class StringEx11_정답 {
 		
 		String[][] items = new String[100][2];
 		for(int i=0; i<items.length; i++) {
-			items[i][0] = "";
-			items[i][1] = "";
+			items[i][0] = ""; // 카테고리
+			items[i][1] = ""; // 아이템
 		}
-		
-		int itemCount = 0;
-		
+		//		0				1
+		//i {카테고리, 아이템/아이템/아이템}
+		//...
+
 		int cnt = 0;
 		
-		String[] cate = new String[0];
-		String[] itm = new String[0];
 		String itmMenu = "";
 		
 		while(true) {
-			
-			System.out.println(Arrays.toString(cate));
-			System.out.println(Arrays.toString(itm));
+
 			//System.out.println(Arrays.toString(items));
 			System.out.println("================");
 			
@@ -65,75 +61,27 @@ public class StringEx11_정답 {
 			if (sel == 1) { //카테고리 관리
 
 				System.out.print("카테고리 추가: ");
-				String name1 = scan.next();//입력받기
-				
-				if(cnt == 0) { // 처음
-					cate = new String[1];
-					cate[0] = name1; 
-				}
-				else if(cnt >= 1) { // 3
-					
-					String temp[] = cate; // 기존 cate 저장
-					cate = new String[cnt+1]; // 1개 추가 3+1 = 0 1 2 3
-					
-					for(int i=0; i<cnt; i++) { // 3
-						cate[i] = temp[i];
-					}
-					
-					cate[cnt] = name1;
-					temp = null;
-				}
+				String cate = scan.next();//입력받기
+				items[cnt][0] = cate;
 				cnt++;
+				
 			}
 			else if (sel == 2) { //아이템 관리
-				System.out.println("선택할 카테고리: ");
-				String subName1 = scan.next();
-				
-				for(int i=0; i<cate.length; i++) {
-					
-					if(cate[i].equals(subName1)) { //일치하는 카테고리 있는 경우
-					
-						System.out.print("아이템 입력: ");
-						String name2 = scan.next(); // 추가될 아이템명
-
-						String temp[] = itm; // 기존 item
-						itm = new String[itemCount+1]; //3+1 = 0 1 2 3
-						
-						for(int j=0; j<itemCount; j++) {
-							itm[i] = temp[i];
-						}
-						
-						itm[itemCount] = name2; // item 추가
-						
-						temp = null;
-						itemCount++;
-						//카테고리랑 연결
-//				 		{"과자", "홈런볼/쪼리퐁/"},
-//						items = new String[100][2];
-						for(int j=0; j<itemCount; j++) {
-							
-							items[j][0] = cate[i]; //카테고리 cate[i]
-							
-							for(int k=0; k<itemCount; k++) {
-								itmMenu += itm[k]+"/"; //아이템목록 문자열화
-							}
-
-							items[j][1] = itmMenu;
-							
-							System.out.print(items[j][0] + ", ");
-							System.out.print(items[j][1]);
-							  
-						}							
-
-					}
-					
+				for(int i=0; i<cnt; i++) {
+					System.out.println(i + items[i][0]);
 				}
-				System.out.println();
+				System.out.print("카테고리를 선택하세요 : ");
+				int temp = scan.nextInt();
+				
+				System.out.print("추가할 아이템 입력 : ");
+				String item = scan.next();
+				
+				items[temp][1] += item + "/";
 			}
 			else if (sel == 3) { //전체 상품 출력
-				
-		
-				
+				for(int i=0; i<cnt; i++) {
+					System.out.println(items[i][0] + " : " + items[i][1]);
+				}
 			}
 			
 		}
